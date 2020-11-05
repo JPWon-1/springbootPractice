19425fd78e5917d21673f332e577605a705fe12d
JPJPJP <rocknzero1@gmail.com>
2020-11-06 07:52:36 +0900

    페이징 처리

--- src/main/java/com/example/practice/Controller/BoardController.java
+++ src/main/java/com/example/practice/Controller/BoardController.java
@@ -1,7 +1,5 @@
 package com.example.practice.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import com.example.practice.model.Board;
@@ -9,6 +7,10 @@ import com.example.practice.repository.BoardRepository;
 import com.example.practice.validator.BoardValidator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,10 +29,18 @@ public class BoardController {
     @Autowired
     private BoardValidator boardValidator;
 
-
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model,
+            @PageableDefault(size = 10, direction = Direction.DESC, sort = "id") Pageable pageable,
+            @RequestParam(required = false, defaultValue = "") String searchText) {
+        // Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+
+        double pageSize = 10.0;
+        int startPage = Math.min(1, 1);
+        int endPage = (int) Math.ceil(boards.getTotalElements() / pageSize);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
@@ -47,7 +57,7 @@ public class BoardController {
     }
 
     @PostMapping("/form")
-    public String formSubmit(@Valid  Board board, BindingResult bindingResult) {
+    public String formSubmit(@Valid Board board, BindingResult bindingResult) {
         boardValidator.validate(board, bindingResult);
         if (bindingResult.hasErrors()) {
             return "board/form";
--- src/main/java/com/example/practice/repository/BoardRepository.java
+++ src/main/java/com/example/practice/repository/BoardRepository.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import com.example.practice.model.Board;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Long>{
     List<Board> findByTitle(String title);
     List<Board> findByTitleOrContent(String title,String content);
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
