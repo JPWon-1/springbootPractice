5791dddfeefbaefad9c3ad2d90aefa706fc71505
JPJPJP <rocknzero1@gmail.com>
2020-11-05 21:20:00 +0900

    restfulAPI( CRUD )

--- src/main/java/com/example/practice/repository/BoardRepository.java
+++ src/main/java/com/example/practice/repository/BoardRepository.java
@@ -1,9 +1,12 @@
 package com.example.practice.repository;
 
+import java.util.List;
+
 import com.example.practice.model.Board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Long>{
-    
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title,String content);
 }
